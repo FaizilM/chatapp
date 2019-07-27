98d7266f8fde93c602173b97a0806586b821ad78
Eliyas, Faizil <feliyas@sevone.com>
2019-07-27 14:32:43 +0530

    init

--- public/chat.js
+++ public/chat.js
@@ -1,42 +1,47 @@
 
 $(function(){
-   	//make connection
-	var socket = io.connect('http://localhost:3000')
 
-	//buttons and inputs
-	var message = $("#message")
-	var username = $("#username")
-	var send_message = $("#send_message")
-	var send_username = $("#send_username")
-	var chatroom = $("#chatroom")
-	var feedback = $("#feedback")
+	var server_ip = $("#server_ip")
+	if (server_ip) {
+		//make connection
+		var socket = io.connect('http://'+server_ip+':3000')
 
-	//Emit message
-	send_message.click(function(){
-		socket.emit('new_message', {message : message.val()})
-	})
+		//buttons and inputs
+		var message = $("#message")
+		var username = $("#username")
+		var send_message = $("#send_message")
+		var send_username = $("#send_username")
+		var chatroom = $("#chatroom")
+		var feedback = $("#feedback")
 
-	//Listen on new_message
-	socket.on("new_message", (data) => {
-		feedback.html('');
-		message.val('');
-		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-	})
+		//Emit message
+		send_message.click(function(){
+			socket.emit('new_message', {message : message.val()})
+		})
 
-	//Emit a username
-	send_username.click(function(){
-		socket.emit('change_username', {username : username.val()})
-	})
+		//Listen on new_message
+		socket.on("new_message", (data) => {
+			feedback.html('');
+			message.val('');
+			chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+		})
 
-	//Emit typing
-	message.bind("keypress", () => {
-		socket.emit('typing')
-	})
+		//Emit a username
+		send_username.click(function(){
+			socket.emit('change_username', {username : username.val()})
+		})
 
-	//Listen on typing
-	socket.on('typing', (data) => {
-		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-	})
+		//Emit typing
+		message.bind("keypress", () => {
+			socket.emit('typing')
+		})
+
+		//Listen on typing
+		socket.on('typing', (data) => {
+			feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+		})
+	}
+   	
 });
 
 
